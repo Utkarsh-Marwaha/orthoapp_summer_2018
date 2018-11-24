46be82b0cdbfbe2eb06558d423a932c625512ac2
Chengwu  Shi <u6165361@anu.edu.au>
2018-11-24 13:03:49 +1100

    rectified enum types, and added new string functions

--- orthoapp/first_app/models.py
+++ orthoapp/first_app/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
+
+
 # Create your models here.
 class Person(models.Model):
     firstName    = models.CharField(max_length=100)
@@ -11,10 +13,14 @@ class Person(models.Model):
     class Meta:
         abstract = True
 
+    def __str__(self):
+        return self.firstName+" "+self.lastName
+
 class Surgeon(Person):
     hospital_name = models.CharField(max_length = 264)
 
 class Patient(Person):
+
     KNEE = 'K'
     HIP  = 'H'
     OPERATION_TYPE_CHOICES =(
@@ -40,9 +46,9 @@ class Patient(Person):
 
 
     surgeon = models.ForeignKey(Surgeon, on_delete=models.CASCADE, related_name="patients")
-    operationType = models.CharField(max_length = 1)
-    operationSide = models.CharField(max_length = 1)
-    surgeryType   = models.CharField(max_length = 1)
+    operationType = models.CharField(max_length = 1, choices = OPERATION_TYPE_CHOICES)
+    operationSide = models.CharField(max_length = 1, choices = OPERATION_SIDE_CHOICES)
+    surgeryType   = models.CharField(max_length = 1, choices = SURGERY_TYPE_CHOICES)
     surgeryDate = models.DateField()
     dateOfBirth = models.DateField()
 
@@ -54,12 +60,18 @@ class DataPoint(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     class Meta:
         abstract = True
+
+    def __str__(self):
+        return str(self.created)
+
 class StepCounter(DataPoint):
     steps = models.IntegerField(default=0)
 
 class KneeMotionRange(DataPoint):
     bend = models.FloatField(default=90)
     stretch = models.FloatField(default=90)
+
+
 class PainLevel(DataPoint):
     painLevel = models.IntegerField(
     default=0,
