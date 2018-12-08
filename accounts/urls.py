e983770f51dfc4ce4d7aa2292ae4478fd2027510
u6146791 <u6146791@anu.edu.au>
2018-12-08 16:23:29 +1100

    fixed url paths for all user-types, dashboard needs fixing

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,8 +4,13 @@ from . import views
 
 
 urlpatterns = [
-    path('user_login/', views.user_login, name = 'user_login'),
-    path('signup/', views.SignUpView.as_view(), name = 'signup'),
-    path('signup/surgeon', views.register_surgeon, name = 'signup_surgeon'),
-    path('signup/patient', views.register_patient, name = 'signup_patient'),
+    path('user_login/', views.user_login, name='user_login'),
+    path('user_login/patient', views.patient, name='patient'),
+    path('user_login/surgeon', views.surgeon, name='surgeon'),
+    path('user_login/practice', views.practice, name='practice'),
+    path('user_login/filter_user', views.filter_user, name='filter_user'), 
+    # the above url does not get displayed ever
+    # it is just present so that we can use the filter_user function from the views.py of the accounts app
+    path('signup/surgeon', views.register_surgeon, name='signup_surgeon'),
+    path('signup/patient', views.register_patient, name='signup_patient'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -13,11 +13,11 @@ def filter_user(request):
     print("YOU HAVE ENTER THE INDEX FILTER")
     if request.user.is_authenticated:
         if request.user.is_surgeon:
-            return render(request, 'accounts/surgeon.html')
+            return redirect('surgeon')
         elif request.user.is_patient:
-            return render(request, 'accounts/patient.html')
+            return redirect('patient')
         else:
-            return render(request, 'accounts/signup.html')
+            return redirect('practice')
     return render(request, 'pages/index.html', {})
 
 
@@ -176,14 +176,7 @@ def user_login(request):
             if user.is_active and (user.is_surgeon or user.is_patient or user.is_practice):
                 login(request, user)
                 # return HttpResponseRedirect(reverse('index')) #if its everything ok with the login and password, you will log in and be redirected to the index page
-                if request.user.is_authenticated:
-                    if request.user.is_surgeon:
-                        return render(request, 'accounts/surgeon.html')
-                    elif request.user.is_patient:
-                        return render(request, 'accounts/patient.html')
-                    else:
-                        return render(request, 'accounts/signup.html')
-                return render(request, 'pages/index.html', {})
+                return redirect('filter_user')
 
             else:
                 return HttpResponse("ACCESS DENIED!")
@@ -192,10 +185,15 @@ def user_login(request):
             print("Username: {} and password {}".format(username, password))
             return HttpResponse("Invalid login details supplied!")
     else:
-        return render(request,'accounts/login.html',{})
+        return render(request, 'accounts/login.html',{})
 
-################## new section here ############################
-from django.views.generic import TemplateView
-from django.contrib.auth.mixins import LoginRequiredMixin
-class SignUpView(LoginRequiredMixin, TemplateView):
-    template_name = 'accounts/signup.html'
+
+def patient(request):
+    return render(request, 'accounts/patient.html')
+
+def surgeon(request):
+    return render(request, 'accounts/surgeon.html')
+
+def practice(request):
+    return render(request, 'accounts/signup.html')
+
