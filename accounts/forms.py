fec160716211ee231c2ac49038e08799885b5520
u6146791 <u6146791@anu.edu.au>
2018-12-08 13:44:22 +1100

    forms.py added successfully

---
+++ accounts/forms.py
@@ -0,0 +1,26 @@
+############################## New section here    ######################################
+from django import forms
+from accounts.models import MyUser, Patient, Surgeon
+
+class UserForm(forms.ModelForm):
+
+    password = forms.CharField(widget = forms.PasswordInput())
+
+    class Meta():
+        model = MyUser
+        fields = ('username', 'email', 'password', 'first_name', 'last_name')
+
+class PatientProfileInfoForm(forms.ModelForm):
+
+    class Meta():
+        model = Patient
+        fields = ('dateOfBirth',)
+
+class SurgeonProfileInfoForm(forms.ModelForm):
+
+    class Meta():
+        model = Surgeon
+        fields = ('hospital_name',)
+
+
+################################ second new section here ######################################
