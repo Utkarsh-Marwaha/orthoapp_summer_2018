26c6b5ed10b3e22a003f91696a80860cda9b74cd
Shayne Russell Flint <u4022606@orthoapp.cecs.anu.edu.au>
2018-12-13 17:03:06 +1100

    fixed path to home

--- orthoapp/scripts/production.sh
+++ orthoapp/scripts/production.sh
@@ -1,4 +1,4 @@
-export ORTHOAPP_HOME=/home/users/u4022606/orthoapp_summer_2018/orthoapp
+export ORTHOAPP_HOME=/home/orthoapp/orthoapp_summer_2018/orthoapp
 export DJANGO_DEBUG=True
 export DJANGO_SECRET_KEY='@icr2u*zmks0h9x!$=1u$-@&^mv5&w3wm5$o=g!7sijz9b5@)3'
 export ORTHOAPP_PASSWORD='wehavegotthis'
