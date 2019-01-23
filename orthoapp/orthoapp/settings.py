d220b9c289dd26c191d91dcfd7ddc21ed66f6baa
Chengwu  Shi <u6165361@anu.edu.au>
2019-01-23 19:55:28 +1100

    fixed the operation naming convention

--- orthoapp/orthoapp/settings.py
+++ orthoapp/orthoapp/settings.py
@@ -149,11 +149,11 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL  = '/media/'
 
-# Messages 
+# Messages
 from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
-    
+
 }
 
 #added because i have a model extends "AbstractUser"
