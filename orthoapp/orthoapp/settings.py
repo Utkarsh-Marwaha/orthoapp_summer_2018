76eb22b0b056a1bc9bf34c6a9f0ff2f5a2f6fa0e
Utkarsh Marwaha <u6146791@anu.edu.au>
2019-01-25 22:16:47 +1100

    setup proper email backend

--- orthoapp/orthoapp/settings.py
+++ orthoapp/orthoapp/settings.py
@@ -160,9 +160,9 @@ MESSAGE_TAGS = {
 AUTH_USER_MODEL = "accounts.MyUser"
 
 # EMAIL configuration settings
-
-EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'orthoapp.feedback@gmail.com'
 EMAIL_HOST_PASSWORD = 'wehavegotthis'
