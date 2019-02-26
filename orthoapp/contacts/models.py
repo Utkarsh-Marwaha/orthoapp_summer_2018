20f42f13ab46c9adfcb8bc034efa55d7d2d85893
Utkarsh Marwaha <u6146791@anu.edu.au>
2019-02-26 20:38:25 +1100

    added comments to contacts app

--- orthoapp/contacts/models.py
+++ orthoapp/contacts/models.py
@@ -1,16 +1,25 @@
 from django.db import models
 from datetime import datetime
 
-# Create your models here.
 class Contact(models.Model):
-    info_page = models.CharField(max_length=200)
-    info_page_id = models.IntegerField()
-    name  = models.CharField(max_length=200)
-    email = models.CharField(max_length=100)
-    phone = models.CharField(max_length=100)
-    message = models.TextField(blank=True)
-    contact_date = models.DateTimeField(default=datetime.now, blank=True)
-    user_id = models.IntegerField(blank=True)
 
+    # This is the title of the information page pertaining to which the issue has been reported by the visitor
+    info_page    = models.CharField(max_length=200)
+    # This is the ID of the information page pertaining to which the issue has been reported by the visitor
+    info_page_id = models.IntegerField()
+    # The name of the visitor who reported the issue about the information page
+    name         = models.CharField(max_length=200)
+    # The email of the visitor who reported the issue about the information page
+    email        = models.CharField(max_length=100)
+    # The phone number of the visitor who reported the issue about the information page
+    phone        = models.CharField(max_length=100)
+    # The issue as described by the visitor
+    message      = models.TextField(blank=True)
+    # The date at which the issue was reported by the visitor
+    contact_date = models.DateTimeField(default=datetime.now, blank=True)
+    # The id of the visitor (if she was a user)
+    user_id      = models.IntegerField(blank=True)
+
+    # For easy access and greater readability, the contact shall be displayed by the visitor's name
     def __str__(self):
         return self.name
