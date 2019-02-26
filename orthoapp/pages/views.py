c782594940c9941e94697adf8e8fb64ad8bccd20
Utkarsh Marwaha <u6146791@anu.edu.au>
2019-02-26 21:48:11 +1100

    added comments on pages and info pages apps

--- orthoapp/pages/views.py
+++ orthoapp/pages/views.py
@@ -6,12 +6,13 @@ from information_pages.choices import surgery_stage_choices, hospital_name_choic
 from information_pages.models import Information_Page
 from developer.models import Developer
 
-# Create your views here.
 def index (request):
+
+    # Fetch all the information pages from the backend which are published and are key links. Order them by their title
     info_pages = Information_Page.objects.all().filter(is_published = True, is_key_link = True)[:3]
 
     context = {
-        'info_pages' : info_pages,
+        'info_pages'            : info_pages,
         'surgery_stage_choices' : surgery_stage_choices,
         'hospital_name_choices' : hospital_name_choices
     }
@@ -20,7 +21,7 @@ def index (request):
 
 def about (request):
     # Get all team-members
-    team_members = Developer.objects.all();
+    team_members = Developer.objects.all()
 
     #Get the team member who will be handling the inquiries about pages
     lead_contacts = Developer.objects.all().filter(is_lead_contact= True)
