7842aa54c0afb8bb4d14728a87601da7abcef0fd
aertho <alec.thomas@repositpower.com>
2019-03-08 23:10:06 +1100

    Added test to test that profile page contains patient details.

--- orthoapp/accounts/tests.py
+++ orthoapp/accounts/tests.py
@@ -10,13 +10,14 @@ from .models import Patient
 class PatientTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.patient_creds = {'username': 'test_patient1', 'password': 'test1234567'}
+        cls.patient_creds = {'username': 'test_patient1', 'password': 'test1234567',
+                             'first_name': 'Alec', 'last_name': 'Thomas', 'email': 'alecthomas@gmail.com'}
         user = MyUser.objects.create_user(**cls.patient_creds)
         user.is_patient = True
         user.is_surgeon = False
         user.is_practice = False
         user.save()
-        patient = Patient(user=user, dateOfBirth=datetime.datetime(2000, 1, 1), gender=1)
+        patient = Patient(user=user,  dateOfBirth=datetime.datetime(2000, 1, 1), gender=1)
         patient.save()
 
 
@@ -44,5 +45,12 @@ class ProfilePageTestCase(PatientTestCase):
         self.assertEqual(r.status_code, 200)
         self.client.logout()
 
-
-
+    def test_page_contains_personal_details(self):
+        self.client.login(**self.patient_creds)
+        r = self.client.get('/accounts/user_login/patient/profile')
+        self.assertContains(r, "First Name")
+        self.assertContains(r, self.patient_creds['first_name'])
+        self.assertContains(r, "Last Name")
+        self.assertContains(r, self.patient_creds['last_name'])
+        self.assertContains(r, "Email")
+        self.assertContains(r, self.patient_creds['email'])
